335e71356cd508bb2cdb3188ba1320ec5fe1ddf1
unknown <ariful4966@gmail.com>
2020-02-29 02:29:33 +0600

    Designe Complate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,16 @@
+
+//Login Button Event Handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function(){
     const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
     const transactionArea = document.getElementById("transaction-area");
     transactionArea.style.display = "block";
-})   
+});
+
+// Deposite button Event handler
+const depositeBtn = document.getElementById("addDeposit");
+depositeBtn.addEventListener("click", function(){
+   const depositeAmount = document.getElementById("depositeAmount").value; 
+   const depositeNumber = parseFloat(depositeAmount);
+});
