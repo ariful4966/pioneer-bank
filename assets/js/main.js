de1a16f6b0035c2571e9afb9f4254174da7e3235
unknown <ariful4966@gmail.com>
2020-02-29 08:16:46 +0600

    add Deposite and Current Balance section

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,4 +13,19 @@ const depositeBtn = document.getElementById("addDeposit");
 depositeBtn.addEventListener("click", function(){
    const depositeAmount = document.getElementById("depositeAmount").value; 
    const depositeNumber = parseFloat(depositeAmount);
+
+   const currentDeposit = document.getElementById("currentDeposit").innerText; 
+   const currentDepositNumber = parseFloat(currentDeposit);
+   const totalDeposit = currentDepositNumber + depositeNumber;
+
+   document.getElementById("currentDeposit").innerText = totalDeposit;
+   document.getElementById("depositeAmount").value = "";
+
+   const currentBalance = document.getElementById("currentBalance").innerText;
+   const currentBalanceNumber = parseFloat(currentBalance);
+   const totalBalance = currentBalanceNumber + totalDeposit;
+   document.getElementById("currentBalance").innerText = totalBalance;
+
+
+
 });
